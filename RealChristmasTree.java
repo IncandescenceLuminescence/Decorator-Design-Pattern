390cf22bc1b895375631c58411a4de7b017a0f51
IncandescenceLuminescence <60149069+IncandescenceLuminescence@users.noreply.github.com>
2020-02-04 11:21:33 -0500

    Delete RealChristmasTree.java

--- RealChristmasTree.java
+++
@@ -1,9 +0,0 @@
-package csce247.assignments.decorator;
-public class RealChristmasTree extends ChristmasTree{
-	public RealChristmasTree() {
-		this.description="Real Fir Christmas Tree";
-	}
-	public double getCost() {
-		return 40;
-	}
-}
