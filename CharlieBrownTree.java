b644691ccab27484509f0bf3fa497653626c0d12
IncandescenceLuminescence <60149069+IncandescenceLuminescence@users.noreply.github.com>
2020-02-04 11:21:21 -0500

    Delete CharlieBrownTree.java

--- CharlieBrownTree.java
+++
@@ -1,9 +0,0 @@
-package csce247.assignments.decorator;
-public class CharlieBrownTree extends ChristmasTree{
-	public CharlieBrownTree() {
-		this.description="Charlie Brown's Christmas Tree";		
-	}
-	public double getCost() {
-		return 5;
-	}
-}
