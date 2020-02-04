0237750ad334b1ed3d1ad751c8ecb81f4665e57d
IncandescenceLuminescence <60149069+IncandescenceLuminescence@users.noreply.github.com>
2020-02-04 11:21:41 -0500

    Delete ChristmasTree.java

--- ChristmasTree.java
+++
@@ -1,8 +0,0 @@
-package csce247.assignments.decorator;
-public abstract class ChristmasTree {
-	protected String description;
-	public String toString() {
-		return description;
-	}
-	public abstract double getCost();
-}
