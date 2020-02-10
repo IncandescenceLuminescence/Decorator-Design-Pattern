ecd9c1a2887d29de4bb421825a9e5f22a8f3d824
IncandescenceLuminescence <60149069+IncandescenceLuminescence@users.noreply.github.com>
2020-02-10 00:13:06 -0500

    Add files via upload

---
+++ FakeChristmasTree.java
@@ -0,0 +1,10 @@
+package csce247.assignments.decorator;
+public class FakeChristmasTree extends ChristmasTree{
+	public FakeChristmasTree() {
+		this.description="Artificial Christmas Tree";		
+	}
+	public double getCost() {
+		return 50;
+	}
+	
+}
---
+++ RealChristmasTree.java
@@ -0,0 +1,9 @@
+package csce247.assignments.decorator;
+public class RealChristmasTree extends ChristmasTree{
+	public RealChristmasTree() {
+		this.description="Real Fir Christmas Tree";
+	}
+	public double getCost() {
+		return 40;
+	}
+}
